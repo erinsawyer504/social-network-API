382bf949277a8d9e5af1c9ee7f17c24c59fc0e8e
erinsawyer504 <erinsawyer504@gmail.com>
2023-04-09 15:30:41 -0400

    Setting up the server.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,14 @@ const { connect, connection } = require('mongoose');
 
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+    process.env.MONGODB_URI || 'mongodb://localhost/social-network-api';
 
 connect(connectionString, {
+    useFindAndModify: false,
     useNewUrlParser: true,  
     useUnifiedTopology: true,
 });
+
+mongoose.set('debug', true);
 
 module.exports = connection;
