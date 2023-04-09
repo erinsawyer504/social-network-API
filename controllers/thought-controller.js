1006eb34a65f584a20b4d82ca85aed4cc861377e
erinsawyer504 <erinsawyer504@gmail.com>
2023-04-09 18:09:49 -0400

    Added todo list for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,12 @@
+const { Thought, User } = require('../models');
+
+//TODO api/thoughts
+    //TODO GET to get all thouths
+    //TODO GET to get a single thought by its _id
+    //TODO POST to create a new thought (dont forget to push the created thought's _id to the associated user's thoughts array field)
+    //TODO put to update a thought by its _id
+    //TODO DELETE to remove a thought by it's _id
+
+//TODO /api/thoughts/:thoughtId/reactions
+    //TODO post to create a reaction stored in a single thoughts reaction array field
+    //TODO DELETE to pull and remove a reaction by the raction's reaction Id value
---
+++ controllers/user-controller.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+
+//TODO /api/users
+    //TODO get all users
+    //TODO get a single user by _id and populated thought and friend data
+    //TODO post a new user
+    //TODO put to update user by its _id
+    //TODO delete to remove user by its _id
+    //TODO Bonus - remove a user's associated thoughts when deleted
+
+//TODO /api/users/:userId/friends/:friendId
+    //TODO post to add a new friend to a user's friend list
+    //TODO delete to remove a friend from a user's friend list
